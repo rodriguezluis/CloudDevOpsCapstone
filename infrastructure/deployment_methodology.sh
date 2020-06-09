a1400b4c469c676cd5841ca1f4294250ea9ebb92
Luis Rodriguez <luifer.dev@outlook.com>
2020-06-08 20:15:50 -0400

    updating deployment script

--- infrastructure/deployment_methodology.sh
+++ infrastructure/deployment_methodology.sh
@@ -1,6 +1,9 @@
+#Deploy the app
 # STEP 1 changing the image:
 kubectl set image deployment/capstone-app capstone-app=nginx:1.16.1 --record
+
 # STEP 2 cheking deployment status:
 kubectl rollout status deployment capstone-app
+
 # STEP 3 list deployment
 kubectl get deployment capstone-app
